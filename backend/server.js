fc60cf55751a460ef933de621499717bc26adacb
hamse-ai <h.jama@alustudent.com>
2025-06-12 16:51:41 +0300

    get received money message

--- backend/server.js
+++ backend/server.js
@@ -18,28 +18,52 @@ db.connect();
 
 app.use(express.urlencoded({ extended: true }));
 
+let receivedMoney;
+
 app.get('/', async (req, res) => {
   res.send('Hello')
-
+  await countReceived()
+  console.log(receivedMoney)
+  console.log(receivedMoney.length)
   
 });
+
+
+// A function that filters the database for messages indicating receiving money and returrns an arrray of those messages
+async function countReceived() {
+  try {
+    const result = await db.query(
+      "SELECT * FROM sms_info WHERE body LIKE '%' || 'You have received' || '%';",
+    )
+    receivedMoney = result.rows
+
+  } catch (error) {
+    console.log('error finding data: ', error)
+  }
+  return receivedMoney 
+}
+
+
+// A function to test if there are aany dublicates in the db the number of records in the table is 1691 if the output is the same number then there are no dublicates
+// async function testunique() {
+//   try {
+//     const uniqueValues = await db.query(
+//       "SELECT DISTINCT body, date FROM sms_info"
+//     )
+//     console.log(uniqueValues.rows.length)
+//   } catch (error) {
+//     console.log('error finding data: ', error)
+//   }
+// }
+
+// // testunique()
+
+
+
+
+
 
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
-
-async function tst() {
-
-  try {
-    const recieved = await db.query(
-      "SELECT * FROM sms_info WHERE body LIKE '%' || 'You have received' || '%';",
-    )
-    console.log(recieved.rows);
-    
-  } catch (error) {
-    console.log('error finding data: ', error)
-  }
-}
-
-tst()
